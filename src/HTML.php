44fa5e6e63bf65df8c8b2cf9b3ef851248a5708e
Tom Adams <tom@dxw.com>
2016-01-27 12:46:59 -0500

    Basic functionality

--- src/HTML.php
+++ src/HTML.php
@@ -6,6 +6,15 @@ trait HTML
 {
     public function assertHTMLEquals($expected, $actual)
     {
-        $this->assertEquals($expected, $actual);
+        $input = [$expected, $actual];
+        $output = [];
+
+        foreach ($input as $val) {
+            $h = new \DOMDocument;
+            $h->loadHTML($val);
+            $output[] = $h->saveHTML();
+        }
+
+        $this->assertEquals($output[0], $output[1]);
     }
 }
--- tests/html_test.php
+++ tests/html_test.php
@@ -12,11 +12,25 @@ class HTMLClass
 
 class HTMLTest extends PHPUnit_Framework_TestCase
 {
-    public function testBasic()
+    public function testPlainText()
     {
         $h = new HTMLClass;
         $h->assertHTMLEquals('a', 'b');
 
-        $this->assertEquals(['a', 'b'], $h->args);
+        $this->assertEquals([
+            "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">\n<html><body><p>a</p></body></html>\n",
+            "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">\n<html><body><p>b</p></body></html>\n",
+        ], $h->args);
+    }
+
+    public function testSimpleHtml()
+    {
+        $h = new HTMLClass;
+        $h->assertHTMLEquals('<a href="aaa">bbb</a>', '<a href ="aaa"  >bbb</a >');
+
+        $this->assertEquals([
+            "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">\n<html><body><a href=\"aaa\">bbb</a></body></html>\n",
+            "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.0 Transitional//EN\" \"http://www.w3.org/TR/REC-html40/loose.dtd\">\n<html><body><a href=\"aaa\">bbb</a></body></html>\n",
+        ], $h->args);
     }
 }
